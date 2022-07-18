6ec0017a089ae00cc3b7577d64b743c7efdd432d
tomer255 <tomer255255@gmail.com>
2022-07-18 14:45:55 +0300

    Add register middlewares

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -1,25 +1,88 @@
+const passwordRequirements = require('../config')["password requirements"];
+const {serverLogger} = require ('../logger')
+const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken");
+
 
 module.exports.login = async (req, res) => {
   const { password, email } = req.body;
-  let sql = "SELECT id,email,password,hashedPassord FROM users where email=(?)"
-  let query = db.query(sql, [email], (error, results, fields));
-
-  query.on("error", function (err) {
-    logger.serverLogger.log('error', err)
-    return res.status(500).send("An error occurred");
-  });
-  query.on("result", function (result) {
-      if (result.length === 0) {
-        return res.status(500).send("user email is incorrect");
-      }
-      const isPasswordValid = await bcrypt.compare(password, result[0].password)
-      if (!isPasswordValid){
-        return res.status(500).send("Password is incorrect");
-      }
-
-    token = jwt.sign({ user: result[0] }, process.env.TOKEN_KEY);
+  const sql = "SELECT id,username,email,hased_password FROM users WHERE email=(?)"
+  db.query(sql, [email], async (error, results) => {
+    if(error) return res.status(500).send("An error occurred");
+    if(!results.length) return res.status(400).send("Email or Password is incorrect");
+    const result = results[0]
+    const isPasswordValid = await bcrypt.compare(password, result.hased_password);
+    if (!isPasswordValid){
+      return res.status(500).send("Email or Password is incorrect");
+    }
+    const {id} = result
+    const token = jwt.sign({id}, process.env.TOKEN_KEY);
     return res.json({ token });
-      
   });
 }
 
+module.exports.verifyPasswordRequirements = async (req, res,next) => {
+  const password = req.body.password
+  const valid = Object.values(passwordRequirements).every(requirement => {
+    let re = new RegExp(requirement.regex)
+    return !requirement.require | re.test(password)
+  });
+  if (!valid) {
+    return res.status(400).send("the password don't pass the minimum requirements")
+  }
+  return next();
+}
+
+module.exports.emailNoExists = async (req, res,next) => {
+  const {email} = req.body
+  const sql = "SELECT id FROM users WHERE email=(?)"
+  db.query(sql, [email],(error, results)=>{
+    if(error) return res.status(500).send("An error occurred");
+    if(results.length) return res.status(400).send("Email can`t be used");
+    next();
+  })
+}
+
+module.exports.hashPassword = async (req, res,next) => {
+  try{
+    req.body.password = await bcrypt.hash(req.body.password, 10)
+    next()
+  }catch(err){
+    serverLogger.error(err)
+    res.status(500).send("An error occurred")
+  }
+}
+
+module.exports.insertNewUserIntoDB = async (req, res,next) => {
+  const { username,email,password } = req.body;
+  const sql = "INSERT INTO users (username, email, hased_password) VALUES (?,?,?)"
+  var query = db.query(sql, [username,email,password])
+  query.on("error", function (err) {
+    serverLogger.error(err)
+    res.status(500).send("There was an error updataing the DB");
+  });
+  query.on("result", function (result) {
+    req.db = {users : result};
+    serverLogger.info(`User id ${result.insertId} was created in the DB`)
+    return next();
+  });
+}
+
+module.exports.insertUsersFoldersIntoDB = async (req, res,next) => {
+  const folderID = req.db.folders.insertId;
+  const userID = req.db.users.insertId;
+  const sql = "INSERT INTO users_folders (folder_id, user_id) VALUES (?,?)"
+  var query = db.query(sql, [folderID,userID])
+  query.on("error", function (err) {
+    serverLogger.error(err)
+    res.status(500).send("There was an error updataing the DB");
+  });
+  query.on("result", function (result) {
+    req.db.UsersFolders = result;
+    return next();
+  });
+}
+
+module.exports.register = async (req, res) => {
+  res.status(200).send("Registration completed successfully")
+}
--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const UsersController = require("../controllers/users");
+const {register,login,emailNoExists,hashPassword,verifyPasswordRequirements,insertNewUserIntoDB,insertUsersFoldersIntoDB} = require("../controllers/users");
+const {insertRootFolderIntoDB} = require("../controllers/folders");
 
-router.post("/register", UsersController.register);
-router.post("/login", UsersController.login);
+
+router.post("/register", verifyPasswordRequirements, emailNoExists, hashPassword, insertNewUserIntoDB, insertRootFolderIntoDB, insertUsersFoldersIntoDB, register);
+router.post("/login", login);
 
 module.exports = router;
