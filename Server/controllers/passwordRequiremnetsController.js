9a0f9c6f8a04ee9d4e530e3dc8439954f657a2df
tomer255 <tomer255255@gmail.com>
2022-07-18 14:42:10 +0300

    change serverLogger syntax

--- Server/controllers/passwordRequiremnetsController.js
+++ Server/controllers/passwordRequiremnetsController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const logger = require ('../logger')
+const {serverLogger} = require ('../logger')
 const passReq = require("../config.json");
 
 
@@ -8,7 +8,7 @@ module.exports.getPassRequirements = async (req, res) => {
   try {
       res.json(passReq["password requirements"]);
   } catch (err) {
-    logger.serverLogger.log('error', err)
+    serverLogger.log('error', err)
     res.status(500).send("unable to fetch the requirements file")
   }
 };
