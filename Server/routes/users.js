79aadc82a488f6c2291d42f726fc23fd985e3eca
tomer255 <tomer255255@gmail.com>
2022-08-05 15:17:07 +0300

    add captcha to login

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {captcha} = require("../controllers/users");
-const {getUserFromDB,verifyPassword,sendToken} = require("../controllers/users");
-const {register,emailNoExists,hashPassword,verifyPasswordRequirements,insertNewUserIntoDB,insertUsersFoldersIntoDB} = require("../controllers/users");
+const {captcha,
+    getUserFromDB,verifyPassword,sendToken,
+    register,emailNoExists,hashPassword,verifyPasswordRequirements,
+    insertNewUserIntoDB,insertUsersFoldersIntoDB} = require("../controllers/users");
 const {insertRootFolderIntoDB} = require("../controllers/folders");
 
-
-router.post("/register",captcha, verifyPasswordRequirements, emailNoExists, hashPassword, insertNewUserIntoDB, insertRootFolderIntoDB, insertUsersFoldersIntoDB, register);
-router.post("/login",captcha,getUserFromDB,verifyPassword,sendToken );
+router.use(captcha)
+router.post("/register", verifyPasswordRequirements, emailNoExists, hashPassword, insertNewUserIntoDB, insertRootFolderIntoDB, insertUsersFoldersIntoDB, register);
+router.post("/login",getUserFromDB,verifyPassword,sendToken );
 
 module.exports = router;
