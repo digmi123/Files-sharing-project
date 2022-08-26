b5dd9b12c58ab598f78764c05b4fd485ce1aa85e
digmi123 <65775879+digmi123@users.noreply.github.com>
2022-08-26 22:10:23 +0300

    Change the input default button to styled one

--- Client/src/components/FilesSection.js
+++ Client/src/components/FilesSection.js
@@ -1,51 +1,71 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import FilesArea from "./FilesArea";
-import {uploadFiles , updateFilesData} from "../functions/ApiCalls"
-import {useSelector , useDispatch} from "react-redux"
+import { uploadFiles, updateFilesData } from "../functions/ApiCalls";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
+import { useEffect } from "react";
 
 function FilesSection() {
-  const dispatch = useDispatch()
-  //Stats: 
-  let {filesData} = useSelector(state => state.filesData)
+  const dispatch = useDispatch();
+  //Stats:
+  let { filesData } = useSelector((state) => state.filesData);
   const [files, setFiles] = useState([]);
-  const [currentFolder,setCurrentFolder] = useState([]);
-  const navigate = useNavigate()
-  
+  const [currentFolder, setCurrentFolder] = useState([]);
+  const hiddenFileInput = React.useRef(null);
+  const navigate = useNavigate();
+
+  //had a warning cannot render a comp while ...
+  //The solution on internet was to inser into useEffect the function were getting from props.
+  //Were not getting function so just log fixed it need to think what to do :(  .
+  useEffect(()=>{
+    console.log(":fgdgdgdf");
+  }, [files])
+
   //Methods:
   const changeHandler = (e) => {
-    setFiles([
-      ...files,
-      ...Array.from(e.target.files)
-    ]);
+    console.log("Im on the change handler of the hidden input.");
+    setFiles([...files, ...Array.from(e.target.files)]);
   };
 
   const submitHandler = async () => {
-    await uploadFiles(currentFolder,files);
-    dispatch(updateFilesData(navigate))
+    await uploadFiles(currentFolder, files);
+    dispatch(updateFilesData(navigate));
   };
 
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
 
   return (
     <Container>
       <ActionsWrapper>
         {/* 3 Buttoons */}
         <FilesSelectionContainer>
+          <Button onClick={handleClick}>Upload a file</Button>
           <FilesInput
             type="file"
             name="file"
+            ref={hiddenFileInput}
             onChange={changeHandler}
             multiple="multiple"
+            style={{ display: "none" }}
           />
         </FilesSelectionContainer>
       </ActionsWrapper>
       {/* <hr width="100vw" /> */}
-        <FilesArea filesData={filesData} back={()=>{console.log("in root")} } path="" setCurrentFolder={setCurrentFolder}/>
+      <FilesArea
+        filesData={filesData}
+        back={() => {
+          console.log("in root");
+        }}
+        path=""
+        setCurrentFolder={setCurrentFolder}
+      />
     </Container>
   );
 }
+
 
 export default FilesSection;
 
@@ -63,8 +83,7 @@ const ActionsWrapper = styled.div`
   align-items: center;
 `;
 
-const FilesInput = styled.input`
-`;
+const FilesInput = styled.input``;
 
 const FilesSelectionContainer = styled.div`
   display: flex;
@@ -76,14 +95,13 @@ const Button = styled.button`
   font-size: 1em;
   margin: 0.25em;
   padding: 0.25em 0.5em;
-  border: 2px solid #5499C7;
-  width: 20%;
+  border: 2px solid #5499c7;
   height: 20%;
   border-radius: 3px;
   background: white;
-  color: #5499C7;
+  color: #5499c7;
   &:hover {
-    background: #5499C7;
+    background: #5499c7;
     color: white;
   }
-`
+`;
