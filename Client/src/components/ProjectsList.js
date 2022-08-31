b91b78cf7d6f321ed26bbdcf141c9c33f62fc45a
tomer255 <tomer255255@gmail.com>
2022-08-31 19:22:03 +0300

    change "Edit Button" to "Button"

--- Client/src/components/ProjectsList.js
+++ Client/src/components/ProjectsList.js
@@ -41,9 +41,9 @@ function ProjectsList() {
                             onClick={() => selectProject(item.project_id)}
                             >{item.name}
                         </Button>
-                        <EditButton onClick={()=>showEditeProject(item)}>
+                        <Button onClick={()=>showEditeProject(item)}>
                             <MdEdit/>
-                        </EditButton>
+                        </Button>
                     </div>
                 ))}
             </ButtonsWrapper>
@@ -67,7 +67,7 @@ const ButtonsWrapper = styled.div`
 
 const Button = styled.button`
   font-size: 1em;
-  margin: 1em;
+  margin: 1em 0.25em;
   padding: 0.25em 0.5em;
   border: 2px solid #5499C7;
   border-radius: 3px;
@@ -77,16 +77,3 @@ const Button = styled.button`
     background: #5499C7;
     color: white;
 }`
-
-const EditButton = styled.button`
-font-size: 1em;
-padding: 0.25em 0.5em;
-border: 2px solid #5499C7;
-border-radius: 3px;
-background: white;
-color: #5499C7;
-&:hover {
-background: #5499C7;
-color: white;
-}
-`
